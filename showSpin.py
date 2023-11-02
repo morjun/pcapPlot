d8a476f69f60a8c8a4de4d54c38a4295337f226d
morjun <junmo2011@gmail.com>
2023-11-02 16:05:52 +0900

    Adapt pcap parsing using pyshark

--- showSpin.py
+++ showSpin.py
@@ -1,3 +1,5 @@
+import pyshark
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -8,36 +10,40 @@ def main():
     times = []
     lostTimes = []
     spins = []
-    with open('serverQUIC-spin3.txt', 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            splitted = line.split('\t')
-            time = splitted[0]
-            spin = splitted[1]
 
-            times.append(float(time))
-            spins.append(int(spin))
+    cap = pyshark.FileCapture("interlop.pcapng", display_filter='quic')
+    initialTime = cap[0].sniff_time.timestamp()
+    for packet in cap:
+        if int(packet.number) > 1000:
+             break
+        if hasattr(packet, 'quic') and hasattr(packet.quic, 'spin_bit'):
+                time = packet.sniff_time.timestamp() - initialTime
+                spin = packet.quic.spin_bit
+                times.append(float(time))
+                spins.append(int(spin))
 
-            print(line)
+                # print(packet)
+
     df = pd.DataFrame({'time': times, 'spin': spins})
 
-    with open("dropLog.txt", "r") as f:
-        lines = f.readlines()
-        for line in lines:
-            if ("RxDrop" in line):
-                splitted = line.split(' ')
-                time = splitted[-1]
-                lostTimes.append(float(time))
-                print(time)
-    
+    # with open("dropLog.txt", "r") as f:
+    #     lines = f.readlines()
+    #     for line in lines:
+    #         if ("RxDrop" in line):
+    #             splitted = line.split(' ')
+    #             time = splitted[-1]
+    #             lostTimes.append(float(time))
+    #             print(time)
+
     fig, ax = plt.subplots(sharex=True, sharey=True)
     fig.set_size_inches(15, 3)
 
     ax.xaxis.set_major_formatter(ticker.FormatStrFormatter('%0.9f'))
     ax.plot(df.time,df.spin, markersize=1,)
-    ax.plot(lostTimes, np.ones(len(lostTimes)), 'r*', markersize=10, label='drop')
-    
+
+    # ax.plot(lostTimes, np.ones(len(lostTimes)), 'r*', markersize=10, label='drop')
     # ax.legend()
+
     plt.xticks(times, rotation = 45)
     plt.yticks([1.0, 0.0])
     plt.xlim(1, 5.0)
