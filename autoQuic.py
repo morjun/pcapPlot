10934f404f4df489c2be30e329fe6833efec5d4e
morjun <junmo2011@gmail.com>
2024-06-15 11:46:15 +0900

    Add code to get stdout

--- autoQuic.py
+++ autoQuic.py
@@ -15,6 +15,18 @@ BANDWIDTH = 17
 class QuicRunner:
     def __init__(self):
         self.server, self.client = self.runContainers()
+    
+    def run_command_in_container(self, container, command):
+            exec_id = self.dockerClient.api.exec_create(container.id, command, stdout=True, stderr=True, stdin=False, tty=False)
+            logs = self.dockerClient.api.exec_start(exec_id, stream=True)
+            
+            # 출력을 실시간으로 받아오기
+            for line in logs:
+                print(line.decode('utf-8').strip())
+
+            # 실행 결과 받기
+            exit_code = self.dockerClient.api.exec_inspect(exec_id)['ExitCode']
+            return exit_code
 
     def runContainers(self):
         self.dockerClient = docker.from_env()
@@ -35,13 +47,13 @@ class QuicRunner:
 
     def runQuic(self, lossRate, delay):
             filename = f"l{lossRate}b{BANDWIDTH}d{delay}"
-            self.server.exec_run(f"tc qdisc add dev eth1 root netem loss {lossRate}% delay {delay}ms rate {BANDWIDTH}mbit")
+            self.run_command_in_container(self.server,f"tc qdisc add dev eth1 root netem loss {lossRate}% delay {delay}ms rate {BANDWIDTH}mbit")
 
             # 서버 컨테이너에 실행
             exec_id = self.dockerClient.api.exec_create(self.server.id, f"tcpdump -s 0 -i eth1 -w {filename}.pcap & ./scripts/log_wrapper.sh ./artifacts/bin/linux/x64_Release_openssl/quicsample -server -cert_file:./artifacts/bin/linux/x64_Debug_openssl/cert.pem -key_file:./artifacts/bin/linux/x64_Debug_openssl/priv.key --gtest_filter=Basic.Light", stdin=True, tty=True)
             sock = self.dockerClient.api.exec_start(exec_id, socket=True)
 
-            self.client.exec_run(f"./artifacts/bin/linux/x64_Release_openssl/quicsample -client -unsecure -target:{self.serverIp}")
+            self.run_command_in_container(self.client,f"./artifacts/bin/linux/x64_Release_openssl/quicsample -client -unsecure -target:{self.serverIp}")
 
             # 클라이언트 실행 종료 시 엔터 키 전송
             sock._sock.send(b'\n')
@@ -52,11 +64,11 @@ class QuicRunner:
                     print("Server has exited.")
                     break
             
-            self.server.exec_run(f"mv {filename}.pcap /msquic_logs")
-            self.server.exec_run(f"msquic_lttng0/quic.log /msquic_logs/{filename}.log")
-            self.server.exec_run(f"rm -rf msquic_lttng0")
+            self.run_command_in_container(self.server,f"mv {filename}.pcap /msquic_logs")
+            self.run_command_in_container(self.server,f"msquic_lttng0/quic.log /msquic_logs/{filename}.log")
+            self.run_command_in_container(self.server,f"rm -rf msquic_lttng0")
 
-            self.server.exec_run(f"tc qdisc del dev eth1 root")
+            self.run_command_in_container(self.server,f"tc qdisc del dev eth1 root")
 
 def main():
     lastValues = []
