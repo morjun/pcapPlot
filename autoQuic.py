0135a6c4c0ab62e85b84f0a0713b722350ecbfb6
morjun <junmo2011@gmail.com>
2024-11-14 15:05:15 +0900

    add export tls key

--- autoQuic.py
+++ autoQuic.py
@@ -180,7 +180,7 @@ class QuicRunner:
 
         # command = f"tcpdump -s 262144 -i eth1 -w {filename}.pcap & ./scripts/log_wrapper.sh ./artifacts/bin/linux/x64_Debug_openssl/quicsample -server -cert_file:./artifacts/bin/linux/x64_Debug_openssl/cert.pem -key_file:./artifacts/bin/linux/x64_Debug_openssl/priv.key --gtest_filter=Basic.Light"
         commands = [
-            f"tshark -i eth1 -w {filename_ext}.pcap",
+            f"tshark -i eth1 -w {filename_ext}.pcap --export-tls-session-keys {filename_ext}.key",
             "./scripts/log_wrapper.sh ./artifacts/bin/linux/x64_Debug_openssl/quicsample -server -cert_file:./artifacts/bin/linux/x64_Debug_openssl/cert.pem -key_file:./artifacts/bin/linux/x64_Debug_openssl/priv.key --gtest_filter=Basic.Light",
         ]
 
