22735c5ff66c400de603a3e3d8e78799ab40a332
morjun <junmo2011@gmail.com>
2025-02-26 14:14:36 +0900

    Edit script

--- lost_gnuplot.sh
+++ lost_gnuplot.sh
@@ -4,11 +4,11 @@
 INPUT_FILE=$1
 OUTPUT_FILE=$2
 
-# Gnuplot 스크립트 생성
+# Gnuplot 스크립트 실행
 gnuplot <<- EOF
     set terminal pngcairo size 800,600
     set output "$OUTPUT_FILE"
-
+    
     set title "Loss Detection"
     set xlabel "time (sec.)"
     set ylabel "Signals"
@@ -18,7 +18,10 @@ gnuplot <<- EOF
     # Define the plot styles
     set style line 1 lc rgb 'purple' pt 7 ps 1.5   # PROBE, FACK, RACK color and point styles
 
-    plot "$INPUT_FILE" using 1:2 with points linestyle 1 title 'PROBE', \
-         "$INPUT_FILE" using 1:3 with points linestyle 1 title 'FACK', \
-         "$INPUT_FILE" using 1:4 with points linestyle 1 title 'RACK'
+    # Time and loss data; 
+    # plot loss column where 0 corresponds to RACK, 1 to FACK, and 2 to PROBE
+    plot "$INPUT_FILE" using 1:($2==0 ? 1/0 : 1/1) with points linestyle 1 title 'RACK', \
+         "$INPUT_FILE" using 1:($2==1 ? 1/0 : 1/1) with points linestyle 1 title 'FACK', \
+         "$INPUT_FILE" using 1:($2==2 ? 1/0 : 1/1) with points linestyle 1 title 'PROBE'
 EOF
+
